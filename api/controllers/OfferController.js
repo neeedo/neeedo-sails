e481599d26f05039ac76cc6538030a0723479003
Sascha Feldmann <sascha.feldmann@gmx.de>
2015-05-13 07:44:08 +0200

    #424176 implemented offer update + delete controller actions

--- api/controllers/OfferController.js
+++ api/controllers/OfferController.js
@@ -12,10 +12,9 @@ module.exports = {
         }) + " was created successfully.");
 
       FlashMessagesService.setSuccessMessage('Your offer was created successfully.', req, res);
+      OfferService.storeInSession(req, createdOffer);
 
-      res.view('offer/create', {
-
-      });
+      res.redirect(OfferService.getEditUrl(createdOffer));
     };
 
     var onErrorCallback = function(errorModel) {
@@ -39,11 +38,77 @@ module.exports = {
       res.view('offer/create');
     }
   },
-  update: function (req, res) {
+  edit: function (req, res) {
+    /*
+     * ---------- callbacks ----------
+     */
+    var onSuccessCallback = function(updatedOffer) {
+      sails.log.info("Offer " + util.inspect(updatedOffer, {
+        showHidden: false,
+        depth: null
+      }) + " was created successfully.");
 
+      FlashMessagesService.setSuccessMessage('Your offer was updated successfully.', req, res);
+      OfferService.storeInSession(req, updatedOffer);
+
+      res.view('offer/edit', {
+
+      });
+    };
+
+    var onErrorCallback = function(errorModel) {
+      ApiClientService.logMessages(errorModel);
+      ApiClientService.addFlashMessages(req, res, errorModel);
+
+      res.redirect('/offer/edit');
+    };
+
+    var offerId = req.param("id");
+
+    var offer = OfferService.loadOffer(req, offerId);
+
+    if (undefined == offer) {
+      FlashMessagesService.setErrorMessage('The offer could not be loaded.', req, res);
+      return req.redirect('/');
+    }
+
+    OfferService.updateOffer(offer, onSuccessCallback, onErrorCallback);
   },
   delete: function (req, res) {
+    /*
+     * ---------- callbacks ----------
+     */
+    var onSuccessCallback = function(deletedOffer) {
+      sails.log.info("Offer " + util.inspect(deletedOffer, {
+        showHidden: false,
+        depth: null
+      }) + " was deleted successfully.");
 
+      FlashMessagesService.setSuccessMessage('Your offer was deleted successfully.', req, res);
+      OfferService.removeFromSession(req, deletedOffer);
+
+      res.view('/', {
+
+      });
+    };
+
+    var onErrorCallback = function(errorModel) {
+      ApiClientService.logMessages(errorModel);
+      ApiClientService.addFlashMessages(req, res, errorModel);
+
+      res.redirect('/');
+    };
+
+    var offerId = req.param("id");
+
+    var offer = OfferService.loadOffer(req, offerId);
+
+    if (undefined == offer) {
+      FlashMessagesService.setErrorMessage('The offer could not be loaded.', req, res);
+      return req.redirect('/');
+    }
+
+    OfferService.deleteOffer(offer, onSuccessCallback, onErrorCallback);
   },
   uploadImage: function (req, res) {
 
--- api/services/OfferService.js
+++ api/services/OfferService.js
@@ -28,5 +28,65 @@ module.exports = {
     } catch (e) {
       onErrorCallBack(ApiClientService.newError("createOffer:" + e.message, 'Your inputs were not valid.'));
     }
+  },
+
+  updateOffer: function(offerModel, onSuccessCallback, onErrorCallBack) {
+    try {
+      var offerService = new OfferService();
+      offerService.updateOffer(offerModel, onSuccessCallback, onErrorCallBack);
+    } catch (e) {
+      onErrorCallBack(ApiClientService.newError("updateOffer:" + e.message, 'Your inputs were not valid.'));
+    }
+
+  },
+
+  deleteOffer: function(offerModel, onSuccessCallback, onErrorCallBack) {
+    try {
+      var offerService = new OfferService();
+      offerService.deleteOffer(offerModel, onSuccessCallback, onErrorCallBack);
+    } catch (e) {
+      onErrorCallBack(ApiClientService.newError("deleteOffer:" + e.message, 'Your inputs were not valid.'));
+    }
+
+  },
+
+  storeInSession: function(req, offerModel) {
+    var offerId = offerModel.getId();
+
+    if (! ("offers" in req.session)) {
+      sails.log.info("setting offers in session");
+      req.session.offers = {};
+    }
+
+    req.session.offers[offerId] = offerModel;
+  },
+
+   removeFromSession: function(req, offerModel) {
+     var offerId = offerModel.getId();
+     if (this.isInSession(req, offerModel.getId())) {
+       req.sesseion.offers[offerId] = undefined;
+     }
+   },
+
+  loadOffer: function(req, offerId) {
+    if (this.isInSession(req, offerId)) {
+      return req.session.offers[offerId];
+    }
+
+    return undefined;
+  },
+
+  isInSession: function(req, offerId) {
+    return offerId in req.session.offers && undefined != req.session.offers[offerId];
+  },
+
+  getEditUrl: function(offerModel) {
+    return 'offer/edit/id/' + offerModel.getId();
+  },
+
+  getDeleteUrl: function(offerModel) {
+    return 'offer/delete/id/' + offerModel.getId();
   }
+
+
 };
