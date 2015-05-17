708220a39ee1d54031bcccce9b59a29466b6a962
Sascha Feldmann <sascha.feldmann@gmx.de>
2015-05-17 20:09:25 +0200

    #424176 mirrored controller, services and views from offers to demands

---
+++ api/controllers/DemandController.js
@@ -0,0 +1,154 @@
+var util = require('util');
+
+module.exports = {
+  create: function (req, res) {
+    /*
+     * ---------- callbacks ----------
+     */
+    var onSuccessCallback = function(createdDemand) {
+       sails.log.info("demand " + util.inspect(createdDemand, {
+          showHidden: false,
+          depth: null
+        }) + " was created successfully.");
+
+      FlashMessagesService.setSuccessMessage('Your demand was created successfully.', req, res);
+      DemandService.storeInSession(req, createdDemand);
+
+      res.redirect(DemandService.getEditUrl(createdDemand));
+    };
+
+    var onErrorCallback = function(errorModel) {
+      ApiClientService.logMessages(errorModel);
+      ApiClientService.addFlashMessages(req, res, errorModel);
+
+      res.redirect('/demand/create');
+    };
+
+    /*
+     * ---------- functionality ----------
+     */
+    if ("POST" == req.method) {
+      var mustTags = req.param("mustTags");
+      var shouldTags = req.param("shouldTags");
+      var minPrice = req.param("minPrice");
+      var maxPrice = req.param("maxPrice");
+      var lat = req.param("lat");
+      var lng = req.param("lng");
+      var distance = req.param("distance");
+
+      DemandService.createDemand(mustTags, shouldTags, lat, lng, distance, minPrice, maxPrice, LoginService.getCurrentUser(req), onSuccessCallback, onErrorCallback);
+    } else {
+      res.view('demand/create', {
+        locals: {
+          mustTags: "tag1, tag2,...",
+          shouldTags: "tag1, tag2,...",
+          minPrice: 0,
+          maxPrice: 50,
+          lat: 35.92516,
+          lng: 12.37528,
+          distance: 30,
+          btnLabel: 'Create'
+        }
+      });
+    }
+  },
+  edit: function (req, res) {
+    /*
+     * ---------- callbacks ----------
+     */
+    var onUpdateSuccessCallback = function(updatedDemand) {
+      sails.log.info("Demand " + util.inspect(updatedDemand, {
+        showHidden: false,
+        depth: null
+      }) + " was created successfully.");
+
+      FlashMessagesService.setSuccessMessage('Your Demand was updated successfully.', req, res);
+      DemandService.storeInSession(req, updatedDemand);
+
+      res.redirect(DemandService.getEditUrl(updatedDemand));
+    };
+
+    var onErrorCallback = function(errorModel) {
+      ApiClientService.logMessages(errorModel);
+      ApiClientService.addFlashMessages(req, res, errorModel);
+
+      res.redirect('/demand/edit');
+    };
+
+    var onLoadSuccessCallback = function(loadedDemand) {
+      sails.log.info("Demand " + util.inspect(loadedDemand, {
+        showHidden: false,
+        depth: null
+      }) + " was loaded successfully.");
+
+      if ("POST" == req.method) {
+        var mustTags = req.param("mustTags");
+        var shouldTags = req.param("shouldTags");
+        var minPrice = req.param("minPrice");
+        var maxPrice = req.param("maxPrice");
+        var lat = req.param("lat");
+        var lng = req.param("lng");
+        var distance = req.param("distance");
+
+        DemandService.updateDemand(loadedDemand, mustTags, shouldTags, lat, lng, minPrice, maxPrice, onUpdateSuccessCallback, onErrorCallback);
+      } else {
+        if (undefined == loadedDemand) {
+          FlashMessagesService.setErrorMessage('The demand could not be loaded.', req, res);
+          return res.redirect('/');
+        }
+
+        res.view('demand/edit', {
+          locals: {
+            mustTags: ApiClientService.toTagString(loadedDemand.getMustTags()),
+            shouldTags: ApiClientService.toTagString(loadedDemand.getShouldTags()),
+            minPrice: loadedDemand.getPrice().getMin(),
+            maxPrice: loadedDemand.getPrice().getMax(),
+            lat: loadedDemand.getLocation().getLatitude(),
+            lng: loadedDemand.getLocation().getLongitude(),
+            distance: loadedDemand.getDistance(),
+            btnLabel: 'Edit'
+          }
+        });
+      }
+
+    };
+
+    var demandId = req.param("demandId");
+    DemandService.loadDemand(req, demandId, onLoadSuccessCallback, onErrorCallback);
+  },
+  delete: function (req, res) {
+    /*
+     * ---------- callbacks ----------
+     */
+    var onDeleteSuccessCallback = function(deletedDemand) {
+      sails.log.info("Demand " + util.inspect(deletedDemand, {
+        showHidden: false,
+        depth: null
+      }) + " was deleted successfully.");
+
+      FlashMessagesService.setSuccessMessage('Your demand was deleted successfully.', req, res);
+      DemandService.removeFromSession(req, deletedDemand);
+
+      res.redirect('/');
+    };
+
+    var onErrorCallback = function(errorModel) {
+      ApiClientService.logMessages(errorModel);
+      ApiClientService.addFlashMessages(req, res, errorModel);
+
+      res.redirect('/');
+    };
+
+    var onLoadSuccessCallback = function(loadedDemand) {
+      sails.log.info("Demand " + util.inspect(loadedDemand, {
+        showHidden: false,
+        depth: null
+      }) + " was loaded successfully.");
+
+      DemandService.deleteDemand(loadedDemand, onDeleteSuccessCallback, onErrorCallback);
+    };
+
+    var demandId = req.param("demandId");
+    DemandService.loadDemand(req, demandId, onLoadSuccessCallback, onErrorCallback);
+  }
+}
---
+++ api/services/DemandService.js
@@ -0,0 +1,119 @@
+var apiClient = require('neeedo-api-nodejs-client'),
+    util = require('util');
+
+var Demand = apiClient.models.Demand;
+var DemandService = apiClient.services.Demand;
+
+module.exports = {
+  /**
+   * Create a new demand. See neeedo API documentation for the meaning of the parameters.
+   *
+   * @see https://github.com/neeedo/neeedo-api#create-demand
+   * @param mustTags
+   * @param shouldTags
+   * @param latitude
+   * @param longitude
+   * @param distance
+   * @param minPrice
+   * @param maxPrice
+   * @param user
+   * @param onSuccessCallback
+   * @param onErrorCallBack
+   */
+  createDemand: function(mustTags, shouldTags, latitude, longitude, distance, minPrice, maxPrice, user, onSuccessCallback, onErrorCallBack) {
+    try {
+      var demandModel = new Demand();
+      demandModel.setMustTags(ApiClientService.toTagArray(tags))
+        .setShouldTags(ApiClientService.toTagArray(tags))
+        .setLocation(ApiClientService.newLocation(parseFloat(latitude), parseFloat(longitude)))
+        .setPrice(new Price().setMin(parseFloat(minPrice)).setMax(parseFloat(maxPrice)))
+        .setDistance(distance)
+        .setUser(user);
+
+      var demandService = new DemandService();
+      demandService.createDemand(demandModel, onSuccessCallback, onErrorCallBack);
+    } catch (e) {
+      onErrorCallBack(ApiClientService.newError("createDemand:" + e.message, 'Your inputs were not valid.'));
+    }
+  },
+
+  updateDemand: function(demandModel, mustTags, shouldTags, latitude, longitude, distance, minPrice, maxPrice, onSuccessCallback, onErrorCallBack) {
+    try {
+      demandModel.setMustTags(ApiClientService.toTagArray(tags))
+        .setShouldTags(ApiClientService.toTagArray(tags))
+        .setLocation(ApiClientService.newLocation(parseFloat(latitude), parseFloat(longitude)))
+        .setPrice(new Price().setMin(parseFloat(minPrice)).setMax(parseFloat(maxPrice)))
+        .setDistance(distance);
+
+      var demandService = new DemandService();
+      demandService.updateDemand(demandModel, onSuccessCallback, onErrorCallBack);
+    } catch (e) {
+      onErrorCallBack(ApiClientService.newError("updateDemand:" + e.message, 'Your inputs were not valid.'));
+    }
+
+  },
+
+  deleteDemand: function(demandModel, onSuccessCallback, onErrorCallBack) {
+    try {
+      var demandService = new DemandService();
+      demandService.deleteDemand(demandModel, onSuccessCallback, onErrorCallBack);
+    } catch (e) {
+      onErrorCallBack(ApiClientService.newError("deleteDemand:" + e.message, 'Your inputs were not valid.'));
+    }
+
+  },
+
+  storeInSession: function(req, demandModel) {
+    var demandId = demandModel.getId();
+
+    if (! ("demands" in req.session)) {
+      sails.log.info("storing demand in session: " + util.inspect(demandModel));
+      req.session.demands = {};
+    }
+
+    req.session.demands[demandId] = demandModel;
+  },
+
+   removeFromSession: function(req, demandModel) {
+     var demandId = demandModel.getId();
+     if (this.isInSession(req, demandModel.getId())) {
+       req.session.demands[demandId] = undefined;
+     }
+   },
+
+  loadDemand: function(req, demandId, onLoadCallback, onErrorCallback) {
+    if (this.isInSession(req, demandId)) {
+      try {
+        sails.log.info('Attempt to restore demand with ID ' + demandId + " from session.");
+
+        var demand = new Demand();
+        demand.loadFromSerialized(req.session.demands[demandId])
+          .setUser(LoginService.getCurrentUser(req));
+
+        onLoadCallback(demand);
+      } catch (e) {
+        onErrorCallBack(ApiClientService.newError("loadDemand:" + e.message, 'Could not restore demand from session.'));
+      }
+    } else {
+      // load via API
+      sails.log.info('Attempt to load demand with ID ' + demandId + " via API.");
+
+      var demandService = new DemandService();
+      demandService.load(demandId, LoginService.getCurrentUser(req), onLoadCallback, onErrorCallback);
+    }
+  },
+
+  isInSession: function(req, demandId) {
+    return "demands" in req.session && demandId in req.session.demands && undefined != req.session.demands[demandId];
+  },
+
+  getEditUrl: function(demandModel) {
+    return 'demands/edit/demandId/' + demandModel.getId();
+  },
+
+  getDeleteUrl: function(demandModel) {
+    return 'demands/delete/demandId/' + demandModel.getId();
+  }
+
+
+};
