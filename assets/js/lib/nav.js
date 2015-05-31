085080548551feae3c6f39c820590c92dcccb470
Sebastian Renner <renner.sebastian@gmail.com>
2015-05-31 13:19:51 +0200

    Added Sidebar Toggle Navigation

---
+++ assets/js/lib/nav.js
@@ -0,0 +1,8 @@
+var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+				body = document.body;
+
+			showLeft.onclick = function() {
+				classie.toggle( this, 'active' );
+				classie.toggle( menuLeft, 'cbp-spmenu-open' );
+				disableOther( 'showLeft' );
+			};
