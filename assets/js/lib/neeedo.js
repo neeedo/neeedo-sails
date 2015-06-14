968ba183e4b67f5fbd9db624d04dc7292abe51cf
cneubauern <cneubauern@googlemail.com>
2015-06-14 18:18:16 +0200

    Revert "#435203 Added Minimal Form Interface demo. Added new JS dependencies and less files"
    This reverts commit 5657121a05af719bf72474bfdd9586e6a2d36321.

--- assets/js/lib/neeedo.js
+++ assets/js/lib/neeedo.js
@@ -1,31 +1,5 @@
 /* ##############################################
  *
- *              SINGLE LINE FORM
- *
- * #############################################
- */
-var theForm = document.getElementById( 'offerForm' );
-
-new stepsForm( theForm, {
-  onSubmit : function( form ) {
-    // hide form
-    classie.addClass( theForm.querySelector( '.simform-inner' ), 'hide' );
-
-    /*
-     form.submit()
-     or
-     AJAX request (maybe show loading indicator while we don't have an answer..)
-     */
-
-    // let's just simulate something...
-    var messageEl = theForm.querySelector( '.final-message' );
-    messageEl.innerHTML = 'Thank you! We\'ll be in touch.';
-    classie.addClass( messageEl, 'show' );
-  }
-} );
-
-/* ##############################################
- *
  *              FLASH MESSAGES
  *
  * #############################################
