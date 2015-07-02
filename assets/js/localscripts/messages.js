4515a8e6876ac276ba513695f1825e243fde791d
Christian Neubauer <cneubauern@googlemail.com>
2015-07-02 13:33:12 +0200

    Update messages.js

--- assets/js/localscripts/messages.js
+++ assets/js/localscripts/messages.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   var setUnreadMsgCount = function () {
     $.get("/messages/count")
       .done(function (count) {
-          $(".icon-envelop").text(count);
+          $(".newMsgCount").text(count);
       }
     )
   };
