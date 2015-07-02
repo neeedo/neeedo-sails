232c45c364ffcb3f8086b8373df75b79791a1368
Christian Neubauer <cneubauern@googlemail.com>
2015-07-02 14:02:56 +0200

    toggle newMsgCount

--- assets/js/localscripts/messages.js
+++ assets/js/localscripts/messages.js
@@ -2,8 +2,12 @@ $(document).ready(function () {
   var setUnreadMsgCount = function () {
     $.get("/messages/count")
       .done(function (count) {
+  if(count==0){
+          $(".newMsgCount").hide();
+        }else{
           $(".newMsgCount").text(count);
-      }
+          $(".newMsgCount").show();
+        }      }
     )
   };
 
