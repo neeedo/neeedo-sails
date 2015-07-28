935e5b73c22a1ae9d272c52e8fb065a05be4e4a1
Sebastian Renner <renner.sebastian@gmail.com>
2015-07-28 15:32:43 +0200

    [no-ticket] Removed unused dialog.js file, moved message form validation to frontend validation file

--- assets/js/localscripts/frontendValidation.js
+++ assets/js/localscripts/frontendValidation.js
@@ -53,3 +53,13 @@ var validateRegisterForm = function(){
   }
   return bool;
 };
+
+var validateMessageForm = function(){
+  classie.removeClass(document.getElementById('errorMessage'), 'showError' );
+
+  if(document.getElementById('messageBody').value.toString() == ""){
+    classie.addClass(document.getElementById('errorMessage'), 'showError' );
+    return false;
+  }
+  return true;
+};
--- assets/js/localscripts/messages.js
+++ assets/js/localscripts/messages.js
@@ -21,13 +21,3 @@ $(document).ready(function () {
 
   setUnreadMsgCount();
 });
-
-var validateMessageForm = function(){
-  classie.removeClass(document.getElementById('errorMessage'), 'showError' );
-
-  if(document.getElementById('messageBody').value.toString() == ""){
-    classie.addClass(document.getElementById('errorMessage'), 'showError' );
-    return false;
-  }
-  return true;
-};
